618a6ec2055cef58be7565e558177b6408159fc7
DandinPower <tomhot246@gmail.com>
2022-09-13 14:31:24 +0800

    delete: 刪除不要的檔案

--- Backend/libs/aes.js
+++
@@ -1,8 +0,0 @@
-const aes256 = require('aes256');
-
-let key = '891225'
-var password = 'cs383838';
-var encrypted = aes256.encrypt(key, password);
-var decrypted = aes256.decrypt(key, encrypted);
-
-console.log(encrypted)
